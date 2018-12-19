619bd72f695aafa011646793c081eb3ef23c4f13
Hod Greeley <hod@greeley.org>
2018-12-18 19:44:21 -0800

    Tweak to static interface.

--- src/main/java/com/couchbase/rx/BasicSource.java
+++ src/main/java/com/couchbase/rx/BasicSource.java
@@ -3,7 +3,7 @@ package com.couchbase.rx;
 public class BasicSource<T> {
   protected volatile OnItemListener<? super T> listener;
 
-  public interface OnItemListener<T> { void onItem(T item); }
+  public static interface OnItemListener<T> { void onItem(T item); }
 
   public void setOnItemListener(OnItemListener<? super T> listener) { this.listener = listener; }
 }
